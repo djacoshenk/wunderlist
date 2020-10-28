03f0773f83cb4ab7bf3538a41fc2d5dc953955ea
Danny Jacoshenk <daniel.jacoshenk@gmail.com>
2020-10-28 09:54:25 -0700

    fixed profile loader error

--- src/pages/RestaurantProfilePage/RestaurantProfileLoader/RestaurantProfileLoader.js
+++ src/pages/RestaurantProfilePage/RestaurantProfileLoader/RestaurantProfileLoader.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
+import React, { useContext } from 'react';
 
+import { RestaurantProfileContext } from '../_Context/RestaurantProfileContext';
 import RestaurantLoaderBubbles from '../../../shared/RestaurantLoaderBubbles/RestaurantLoaderBubbles';
 
 import styles from './RestaurantProfileLoader.module.scss';
 
 function RestaurantProfileLoader() {
-  const { state } = useLocation();
+  const { state } = useContext(RestaurantProfileContext);
 
   return (
     <div className={styles['restaurant-prof-loader']}>
-      {state.place && <h3>Finding you more on {state.place.toUpperCase()}</h3>}
+      {state.place.name && (
+        <h3>Finding you more on {state.place.name.toUpperCase()}</h3>
+      )}
       <RestaurantLoaderBubbles />
     </div>
   );
