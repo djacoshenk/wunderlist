34c69af47ded66cd44a0f1ebe9a1d5b30b676fb4
Danny Jacoshenk <daniel.jacoshenk@gmail.com>
2020-12-25 19:30:28 -0800

    remove context providers

--- src/App.js
+++ src/App.js
@@ -7,34 +7,27 @@ import UserRegisterPage from 'pages/UserRegisterPage/UserRegisterPage';
 import RestaurantSearchPage from 'pages/RestaurantSearchPage/RestaurantSearchPage';
 import RestaurantProfilePage from 'pages/RestaurantProfilePage/RestaurantProfilePage';
 
-import LocationURLProvider from 'context/LocationURLContext';
-import CurrentUserProvider from 'context/CurrentUserContext';
-
 export default function App() {
   return (
     <Fragment>
       <BrowserRouter>
-        <CurrentUserProvider>
-          <LocationURLProvider>
-            <Switch>
-              <Route exact path='/'>
-                <HomePage />
-              </Route>
-              <Route exact path='/login'>
-                <UserLoginPage />
-              </Route>
-              <Route exact path='/register'>
-                <UserRegisterPage />
-              </Route>
-              <Route path='/search/:term/:location'>
-                <RestaurantSearchPage />
-              </Route>
-              <Route path='/business/:alias'>
-                <RestaurantProfilePage />
-              </Route>
-            </Switch>
-          </LocationURLProvider>
-        </CurrentUserProvider>
+        <Switch>
+          <Route exact path='/'>
+            <HomePage />
+          </Route>
+          <Route exact path='/login'>
+            <UserLoginPage />
+          </Route>
+          <Route exact path='/register'>
+            <UserRegisterPage />
+          </Route>
+          <Route path='/search/:term/:location'>
+            <RestaurantSearchPage />
+          </Route>
+          <Route path='/business/:alias'>
+            <RestaurantProfilePage />
+          </Route>
+        </Switch>
       </BrowserRouter>
     </Fragment>
   );
