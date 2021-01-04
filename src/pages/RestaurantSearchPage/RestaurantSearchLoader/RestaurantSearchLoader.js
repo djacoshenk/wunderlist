0237d1980148c32c142236ef2b0bd4cbb2377e19
Danny Jacoshenk <daniel.jacoshenk@gmail.com>
2021-01-03 21:05:26 -0800

    remove text from restaurant search loader

--- src/pages/RestaurantSearchPage/RestaurantSearchLoader/RestaurantSearchLoader.js
+++ src/pages/RestaurantSearchPage/RestaurantSearchLoader/RestaurantSearchLoader.js
@@ -1,21 +1,13 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
 
 import RestaurantLoaderBubbles from 'shared/RestaurantLoaderBubbles/RestaurantLoaderBubbles';
 
 import styles from './RestaurantSearchLoader.module.scss';
 
 export default function RestaurantSearchLoader() {
-  const { term, location } = useParams();
-
   return (
-    <div className={styles['main-content-loader']}>
-      <h3>
-        Finding you the best {term.toUpperCase()} in {location}
-      </h3>
-      <div className={styles['main-content-bubble-loader']}>
-        <RestaurantLoaderBubbles />
-      </div>
+    <div className={styles['main-content-bubble-loader']}>
+      <RestaurantLoaderBubbles />
     </div>
   );
 }
