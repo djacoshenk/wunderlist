85f8c8481b19878f6a8be3b9b87cd2403a56fb26
Danny Jacoshenk <daniel.jacoshenk@gmail.com>
2021-01-03 21:04:59 -0800

    add sortBy button component to generate new search results

---
+++ src/pages/RestaurantSearchPage/SortByButton/SortByButton.js
@@ -0,0 +1,74 @@
+import React, { useEffect } from 'react';
+import { useSelect } from 'downshift';
+import { v4 as uuidv4 } from 'uuid';
+import PropTypes from 'prop-types';
+import { useParams } from 'react-router-dom';
+
+import styles from './SortByButton.module.scss';
+
+SortByButton.propTypes = {
+  setSortByParam: PropTypes.func,
+  fetchPlacesSortBy: PropTypes.func,
+};
+
+export default function SortByButton({ setSortByParam, fetchPlacesSortBy }) {
+  const items = ['Best Match', 'Rating', 'Review Count', 'Distance'];
+  const params = useParams();
+
+  const {
+    isOpen,
+    selectedItem,
+    getToggleButtonProps,
+    getLabelProps,
+    getMenuProps,
+    highlightedIndex,
+    getItemProps,
+  } = useSelect({ items });
+
+  useEffect(() => {
+    if (selectedItem === 'Best Match') {
+      setSortByParam('best_match');
+      fetchPlacesSortBy(params, 'best_match');
+    } else if (selectedItem === 'Rating') {
+      setSortByParam('rating');
+      fetchPlacesSortBy(params, 'rating');
+    } else if (selectedItem === 'Review Count') {
+      setSortByParam('review_count');
+      fetchPlacesSortBy(params, 'review_count');
+    } else if (selectedItem === 'Distance') {
+      setSortByParam('distance');
+      fetchPlacesSortBy(params, 'distance');
+    } else {
+      return;
+    }
+  }, [selectedItem, setSortByParam, params, fetchPlacesSortBy]);
+
+  return (
+    <div className={styles['sort-by-main-container']}>
+      <label htmlFor='select' {...getLabelProps()}>
+        Sort By:
+      </label>
+      <div className={styles['sort-by-btn-list-container']}>
+        <button name='select' type='button' {...getToggleButtonProps()}>
+          {selectedItem || 'Best Match'}
+        </button>
+        <ul {...getMenuProps()}>
+          {isOpen &&
+            items.map((item, index) => (
+              <li
+                style={
+                  highlightedIndex === index
+                    ? { backgroundColor: '#e0e7ff' }
+                    : {}
+                }
+                key={uuidv4()}
+                {...getItemProps({ item, index })}
+              >
+                {item}
+              </li>
+            ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
