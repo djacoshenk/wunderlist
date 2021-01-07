cdb2c85f13537e5a03d16db7e74263e87dd94e75
Danny Jacoshenk <daniel.jacoshenk@gmail.com>
2021-01-07 12:11:53 -0800

    convert App to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import UserRegisterPage from 'pages/UserRegisterPage/UserRegisterPage';
 import RestaurantSearchPage from 'pages/RestaurantSearchPage/RestaurantSearchPage';
 import RestaurantProfilePage from 'pages/RestaurantProfilePage/RestaurantProfilePage';
 
-export default function App() {
+export default function App(): React.ReactNode {
   return (
     <Fragment>
       <BrowserRouter>
