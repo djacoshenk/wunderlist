597e0e72e94f023f9cab6674352f989a791bd572
Danny Jacoshenk <daniel.jacoshenk@gmail.com>
2021-01-12 16:40:19 -0800

    initialize Sentry reporting

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import { Provider } from 'react-redux';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
+
+import App from './App';
 
 import store from 'store/index';
 
 import './styles.scss';
+
+Sentry.init({
+  dsn:
+    'https://59b62345d84547a3a52adecf3c9c46ee@o504051.ingest.sentry.io/5590107',
+  autoSessionTracking: true,
+  integrations: [new Integrations.BrowserTracing()],
+
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
