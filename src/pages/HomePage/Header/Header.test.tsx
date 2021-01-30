fa211f6e0d853a62741915c7bc7ee762afcb4ae8
Danny Jacoshenk <daniel.jacoshenk@gmail.com>
2021-01-29 22:48:29 -0800

    add tests for Header component

--- src/pages/HomePage/Header/Header.test.tsx
+++ src/pages/HomePage/Header/Header.test.tsx
@@ -17,6 +17,7 @@ test('component renders image and headings', () => {
 
 test('component is accessible', async () => {
   const { container } = render(<Header />);
+  const results = await axe(container);
 
-  expect(await axe(container)).toHaveNoViolations();
+  expect(results).toHaveNoViolations();
 });
