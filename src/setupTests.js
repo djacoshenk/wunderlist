6aef375e183cfdc6bbf64436ad7ae2e0107514df
Danny Jacoshenk <daniel.jacoshenk@gmail.com>
2021-01-30 23:32:23 -0800

    write geolocation mock function into test set up

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,2 +1,17 @@
 import '@testing-library/jest-dom/extend-expect';
 import 'jest-axe/extend-expect';
+
+const mockedGeolocation = {
+  getCurrentPosition: jest.fn().mockImplementation((success) =>
+    Promise.resolve(
+      success({
+        coords: {
+          latitude: 46.378333,
+          longitude: 13.836667,
+        },
+      })
+    )
+  ),
+};
+
+global.navigator.geolocation = mockedGeolocation;
