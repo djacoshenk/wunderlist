8b7ef2b96f6f919849b6ebbba49a747f9b52da32
Danny Jacoshenk <daniel.jacoshenk@gmail.com>
2021-01-30 23:33:13 -0800

    write test or UserStatusLoader component

--- src/pages/HomePage/UserStatusLoader/UserStatusLoader.test.tsx
+++ src/pages/HomePage/UserStatusLoader/UserStatusLoader.test.tsx
@@ -1,14 +1,38 @@
-import { render as rtlRender } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { axe } from 'jest-axe';
+import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import UserStatusLoader from './UserStatusLoader';
 
+import currentLoadingStatusReducer from 'reducers/currentLoadingStatusReducer';
 import store from 'store/index';
 
+test('component renders with initial state in Redux store', () => {
+  const initialState = {
+    loadingStatus: { loadingMessage: 'Logging In...', isLoading: false },
+  };
+  const rootReducer = combineReducers({
+    loadingStatus: currentLoadingStatusReducer,
+  });
+  const reduxStore = createStore(rootReducer, initialState);
+
+  render(
+    <Provider store={reduxStore}>
+      <BrowserRouter>
+        <UserStatusLoader />
+      </BrowserRouter>
+    </Provider>
+  );
+
+  expect(
+    screen.getByRole('heading', { name: /logging in/i })
+  ).toBeInTheDocument();
+});
+
 test('component is accessible', async () => {
-  const { container } = rtlRender(
+  const { container } = render(
     <Provider store={store}>
       <BrowserRouter>
         <UserStatusLoader />
