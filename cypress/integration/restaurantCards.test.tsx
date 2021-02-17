9fd71685d56daf81df33b65e24790bb5c6287517
Danny Jacoshenk <hello@dannyjaco.me>
2021-02-16 18:09:13 -0800

    Write e2e tests (#58)
    * Delete tests which were failing because of Web APIs
    
    * Delete fixtures from deleted tests
    
    * Refactor logic in UserLoginForm component
    
    * Refactor userLogin command
    
    * Revise saveRestaurantButton test so localStorage persists
    
    * Write test for RestaurantCards component and create API data fixture
    
    * Add more assertions to SearchBar test
    
    * Revise UserLogin test
    
    * Revise UserLogout test

---
+++ cypress/integration/restaurantCards.test.tsx
@@ -0,0 +1,29 @@
+describe('restaurant cards', () => {
+  it('should update the link in the card', () => {
+    cy.intercept(
+      'GET',
+      'https://wft-geo-db.p.rapidapi.com/v1/geo/cities?limit=3&namePrefix=Los%20Angeles,%20CA&countryIds=US',
+      { fixture: 'fakeLocationParamResults.json' }
+    ).as('locationParamResults');
+
+    cy.intercept(
+      'GET',
+      'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=Burgers&location=Los%20Angeles,%20CA&sort_by=best_match&limit=10',
+      { fixture: 'fakeMoreSearchResults.json' }
+    ).as('moreSearchResults');
+
+    cy.visit('/');
+
+    cy.findByRole('link', { name: /burgers/i }).click();
+
+    cy.findByRole('alert');
+
+    cy.findByRole('textbox', { name: /near/i }).type('Los Angeles, CA');
+
+    cy.wait('@locationParamResults');
+
+    cy.findByRole('link', { name: /burgers/i }).click();
+
+    cy.wait('@moreSearchResults');
+  });
+});
