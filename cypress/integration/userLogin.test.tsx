ba4efd51ce8e6734d2051a3360c3204f79b02b17
Danny Jacoshenk <hello@dannyjaco.me>
2021-02-16 18:01:15 -0800

    Revise UserLogin test

--- cypress/integration/userLogin.test.tsx
+++ cypress/integration/userLogin.test.tsx
@@ -1,9 +1,3 @@
-beforeEach(() => {
-  cy.fixture('fakeCurrentUserData').then((user) => {
-    window.localStorage.setItem('registeredUsers', JSON.stringify(user));
-  });
-});
-
 describe('login page', () => {
   it('route to register page and fill out form', () => {
     const fakeUser = {
@@ -11,10 +5,18 @@ describe('login page', () => {
       password: 'password123',
     };
 
+    cy.fixture('fakeCurrentUserData').then((user) => {
+      window.localStorage.setItem('registeredUsers', JSON.stringify(user));
+    });
+
     cy.visit('/');
+
     cy.findByRole('button', { name: /login/i }).click();
+
     cy.findByRole('textbox', { name: /username/i }).type(fakeUser.username);
+
     cy.findByLabelText('password').type(fakeUser.password);
+
     cy.findByRole('button', { name: /login/i }).click();
   });
 });
