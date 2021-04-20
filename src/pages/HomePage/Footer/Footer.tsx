a5a78ae4c5e08855cd239d05a0019b2180ed85c3
Danny Jacoshenk <hello@dannyjaco.me>
2021-04-20 11:25:19 -0700

    Create and style Footer component

---
+++ src/pages/HomePage/Footer/Footer.tsx
@@ -0,0 +1,16 @@
+import styles from './Footer.module.scss';
+
+export default function Footer() {
+  return (
+    <div className={styles['footer-container']}>
+      <p>
+        Please read docs on usage{' '}
+        <a href='https://github.com/djacoshenk/wunderlist#readme'>HERE</a>
+      </p>
+      <p>
+        Request for temporary access to the CORS demo server{' '}
+        <a href='http://cors-anywhere.herokuapp.com/corsdemo'>HERE</a>
+      </p>
+    </div>
+  );
+}
