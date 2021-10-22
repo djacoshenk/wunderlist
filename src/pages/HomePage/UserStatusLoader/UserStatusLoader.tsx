639c77571b736092d0084124394cedff03bec9f5
Danny Jacoshenk <hello@dannyjaco.me>
2021-10-22 14:58:05 -0700

    Refactor useSelector hook in UserStatusLoader component

--- src/pages/HomePage/UserStatusLoader/UserStatusLoader.tsx
+++ src/pages/HomePage/UserStatusLoader/UserStatusLoader.tsx
@@ -1,13 +1,13 @@
-import { useSelector, TypedUseSelectorHook } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import styles from 'pages/HomePage/UserStatusLoader/UserStatusLoader.module.scss';
 import RestaurantLoaderBubbles from 'shared/RestaurantLoaderBubbles/RestaurantLoaderBubbles';
 import { RootState } from 'store/store';
 
-const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
-
 export default function UserLogoutLoader() {
-  const { loadingMessage } = useTypedSelector((state) => state.loadingStatus);
+  const { loadingMessage } = useSelector(
+    (state: RootState) => state.loadingStatus
+  );
 
   return (
     <div className={styles['user-status-loader']}>
